0985f514399ffe0d5dfd5bc292ab0d9cf773bff0
Pamelegoff <161514162+Pamelegoff@users.noreply.github.com>
2024-03-01 19:37:56 +0000

    primeros ejercicios

---
+++ entrega/ejercicios/viernes01.py
@@ -0,0 +1,16 @@
+#Consigna a
+print("Las maquinas me sorprenden con mucha frecuencia")
+#Consigna b
+print()
+#Consigna c
+print("Estoy", "en un", "curso de la UNPA")
+print("Hoy","es Viernes", sep="_")
+#Consigna d - Se imprimen de igual manera
+print("23")
+print(23)
+#Consigna e
+print("Una computadora puede ser llamada \"inteligente\" si logra engañar a una persona haciéndole creer que es un humano")
+#Consigna f
+print("Pamela", "Soledad", "Legoff", "Edad: 28")
+#Consigna g
+print("Pamela", "Soledad", "Legoff", "Edad: 28", sep="_")
